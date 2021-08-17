7349eafcd1f40541913c5f836afeb935f45f6d91
Gift Masha <gftman24@gmail.com>
2021-08-17 19:18:26 +0300

    new dashboard activity

---
+++ app/src/main/java/com/e/mentalhealth/dashboard.java
@@ -0,0 +1,14 @@
+package com.e.mentalhealth;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate ( Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
