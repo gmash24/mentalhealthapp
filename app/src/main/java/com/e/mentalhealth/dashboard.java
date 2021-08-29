b7e37becd2f43e03cb91b0b9d8750d1a9ee32f28
Gift Masha <gftman24@gmail.com>
2021-08-29 23:12:40 +0300

    working navbar

--- app/src/main/java/com/e/mentalhealth/dashboard.java
+++ app/src/main/java/com/e/mentalhealth/dashboard.java
@@ -1,14 +1,33 @@
 package com.e.mentalhealth;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class dashboard extends AppCompatActivity {
-
+private DrawerLayout mdrawerlayout;
+private ActionBarDrawerToggle mtoggle;
     @Override
     protected void onCreate ( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        mdrawerlayout= findViewById(R.id.drawer);
+         mtoggle= new ActionBarDrawerToggle(this, mdrawerlayout,R.string.open,R.string.close);
+         mdrawerlayout.addDrawerListener(mtoggle);
+         mtoggle.syncState();
+         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected ( @NonNull MenuItem item ) {
+       if (mtoggle.onOptionsItemSelected(item)){
+           return true;
+       }
+
+        return super.onOptionsItemSelected(item);
+
     }
 }
