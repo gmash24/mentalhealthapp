c35b2dd6b225337bc36e83c32ec5804bc3276210
Gift Masha <gftman24@gmail.com>
2021-09-01 17:04:51 +0300

    working listview

--- app/src/main/java/com/e/mentalhealth/listview.java
+++ app/src/main/java/com/e/mentalhealth/listview.java
@@ -1,7 +1,11 @@
 package com.e.mentalhealth;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,11 +22,24 @@ public class listview extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
         listView=findViewById(R.id.listview);
+        listview();
+
+    }
+    public void listview(){
+        //cresting an adapter for the listview
+        ArrayAdapter<String> adapter=  new ArrayAdapter<String>(this,R.layout.listdata,NAMES);
+
+        //linking the adapter to the created listview in layout
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick ( AdapterView<?> parent, View view, int position, long id ) {
+                String value= (String) listView.getItemAtPosition(position);
+                Toast.makeText(listview.this, "Position : " + position + "Value : "+ value, Toast.LENGTH_SHORT).show();
 
 
-
-
-        // Enables Always-on
+            }
+        });
 
     }
 }
